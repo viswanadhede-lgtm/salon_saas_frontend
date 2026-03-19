bcca349545a959c70a01f560db1498067a3de9f3
Kumar Viswanadh <viswanadh.ede@gmail.com>
2026-03-19 11:12:09 +0530

    added role owner

--- onboarding.js
+++ onboarding.js
@@ -143,6 +143,7 @@ function submitOnboarding() {
             phone: data.phone,
             password: data.password,
             plan_id: data.plan_id,
+            role: "owner",
 
             company: {
                 name: companyName,
